f5fc172b7c8c71326c2b32ec80353727e26b4706
ptelang <ptelang@gmail.com>
2015-07-12 13:24:20 -0400

    initial version

---
+++ plot1.R
@@ -0,0 +1,12 @@
+
+
+data <- read.table("./household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+power <- as.numeric(subSetData$Global_active_power)
+
+png("plot1.png", width=480, height=480)
+
+hist(power, main="Global Active Power", xlab="Global Active Power (kilowatts)", col="red")
+dev.off()
